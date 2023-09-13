7887b173d7e929032efd40f88c4fefd5d22c17d7
Michael Housh <m-housh@users.noreply.github.com>
2023-09-13 18:52:37 -0400

    Psychrometrics v0.1.11

--- Formula/psychrometrics.rb
+++ Formula/psychrometrics.rb
@@ -2,12 +2,12 @@ class Psychrometrics < Formula
   desc "Command-line tool for calculating psychrometric properties"
   homepage "https://github.com/swift-psychrometrics/psychrometrics-cli"
   url "https://github.com/swift-psychrometrics/psychrometrics-cli.git", branch: "main"
-  version "0.1.10"
+  version "0.1.11"
   license "MIT"
 
   bottle do
-    root_url "https://github.com/swift-psychrometrics/psychrometrics-cli/releases/download/0.1.10"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "f51b7fa7d065097624a756743ea5fa2dfc8814ca00287197ae09518dbcbca89b"
+    root_url "https://github.com/swift-psychrometrics/psychrometrics-cli/releases/download/0.1.11"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "14c70d46d3f554a924c1a1dd04f55727fb915f8a742670372cb208f3afecf006"
   end
 
   depends_on xcode: ["14.2", :build]
