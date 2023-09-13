92575a0b733fa32b9acd4363ea54f49dbe173352
Michael Housh <m-housh@users.noreply.github.com>
2023-09-13 18:06:27 -0400

    Psychrometrics v0.1.8

--- Formula/psychrometrics.rb
+++ Formula/psychrometrics.rb
@@ -1,24 +1,23 @@
 class Psychrometrics < Formula
-  desc "Command-line tool calculating psychrometrics"
+  desc "Command-line tool for calculating psychrometric properties"
   homepage "https://github.com/swift-psychrometrics/psychrometrics-cli"
   url "https://github.com/swift-psychrometrics/psychrometrics-cli.git", branch: "main"
-  version "0.1.0"
+  version "0.1.8"
   license "MIT"
 
   bottle do
-    root_url "https://github.com/m-housh/dots/releases/download/0.1.6"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "8713352da369d0d0491531596d73dfefd4f5ebe4e6b46ccdbd1c71af194fb782"
-    sha256 cellar: :any_skip_relocation, monterey:      "fd139c5691ee6239ddc56b8fb228b914e45a3c3ab77775a8f5f92cf77c16fd74"
+    root_url "https://github.com/swift-psychrometrics/psychrometrics-cli/releases/download/0.1.8"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "fc428c30a8810c9b6198208ee3c84b71b1d9291a946704dc81cbe210229fecc4"
   end
 
   depends_on xcode: ["14.2", :build]
 
   def install
     system "make", "install", "PREFIX=#{prefix}"
-    generate_completions_from_executable(bin/"dots", "--generate-completion-script")
+    generate_completions_from_executable(bin/"psychrometrics", "--generate-completion-script")
   end
 
   test do
-    system "#{bin}/dots" "--version"
+    system "#{bin}/psychrometrics" "--version"
   end
 end
