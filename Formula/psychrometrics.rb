be97111e14ef1c0e2bad3e2ef31b26d5b07b5078
Michael Housh <m-housh@users.noreply.github.com>
2023-09-16 18:22:33 -0400

    Updated for psychrometrics v0.1.11

--- Formula/psychrometrics.rb
+++ Formula/psychrometrics.rb
@@ -2,15 +2,14 @@ class Psychrometrics < Formula
   desc "Command-line tool for calculating psychrometric properties"
   homepage "https://github.com/swift-psychrometrics/psychrometrics-cli"
   url "https://github.com/swift-psychrometrics/psychrometrics-cli.git", branch: "main"
-  version "0.1.1"
+  version "0.1.11"
   license "MIT"
 
   bottle do
-    root_url "https://github.com/swift-psychrometrics/psychrometrics-cli/releases/download/0.1.1"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "36e6e4ece2e9dcd38e62f2cec1e89efabd9ad3b133eff49cddb8fe56cdc295d7"
-    sha256 cellar: :any_skip_relocation, monterey:     "5081e3f689c8cefc168a79d9c97759502e68330ae020eee66ccd67e3a0dfd03f"
-    sha256 cellar: :any_skip_relocation, ventura:      "f4db51dd207b5a1368432ba3ce11cac410a66a77e0679a88ebb7a0a3098f1621"
+    root_url "https://github.com/swift-psychrometrics/psychrometrics-cli/releases/download/0.1.11"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "ed207606373029da6153dd913daa947ba4be0e28ff740771f86a9a5fd020e7f6"
+    sha256 cellar: :any_skip_relocation, monterey:     "395d588e21ade9d8676345cea67908836183d53d12961f45b86b7c07f19a56e9"
+    sha256 cellar: :any_skip_relocation, ventura:      "b2d7306c1ca95ae8b99b2a8af6be2cb49fb65e17ab6476712ac2012ec66efc72"
   end
 
   depends_on xcode: ["14.2", :build]
